293b10b0aa7bc5ed3fd268a3ff1c9e6702c75962
chan-K <kim.haechan@kaist.ac.kr>
2024-05-17 21:18:58 +0900

    NCI1-abaltion2, 3

--- SAGPool/statistic.py
+++ SAGPool/statistic.py
@@ -1,20 +1,22 @@
 import os
 import argparse
 import scipy
-# import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--ablation', type=int, help='0/1/2/3')
-parser.add_argument('--dataset', type=str, help='PROTEINS/NCI1')
-parser.add_argument('--validation', type=bool, default=True)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--ablation', type=int, help='0/1/2/3')
+# parser.add_argument('--dataset', type=str, help='PROTEINS/NCI1')
+# parser.add_argument('--validation', type=bool, default=True)
+# args = parser.parse_args()
 
 def extract_test_accuracy(file):
-    with open(file, "r") as f:
-        s = f.read().split("Test accuracy: ")[1].strip()
-        test_accuracy = float(s)
+    with open(file, 'rb') as f:
+        byte_data = f.read()
+    str_data = byte_data.decode('utf-8')
+    str_data = str_data.split("Test accuracy: ")[1].strip()
+    test_accuracy = float(str_data)
     return test_accuracy
 
 def check_validity(file):
@@ -26,23 +28,30 @@ def check_validity(file):
 
 
 if __name__ == "__main__":
-    dir = f'./logs_ablation{args.ablation}_{args.dataset}'
-    if args.validation:
-        for file in os.listdir(dir):
-            check_validity(os.path.join(dir, file)) 
+    for dataset in ["NCI1"]:
+        for ablation in [2,3]:
+            dir = f'./logs_ablation{ablation}_{dataset}'
+            # if args.validation:
+            #     for file in os.listdir(dir):
+            #         check_validity(os.path.join(dir, file)) 
     
-    test_accuracys = [extract_test_accuracy(os.path.join(dir, file)) for file in os.listdir(dir)]
+            test_accuracys = [extract_test_accuracy(os.path.join(dir, file)) for file in os.listdir(dir)]
+            # statistic, pvalue = scipy.stats.shapiro(test_accuracys)
+            print(f"{dataset}-ablation{ablation}")
+            print("mean: ", np.mean(test_accuracys))
+            print("std: ", np.std(test_accuracys))
+            print()
+            print()
     
-    plt.hist(test_accuracys, color='skyblue', edgecolor='black')
-    plt.title('Histogram')
-    plt.xlabel('Value')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    plt.show()
+    
+    # plt.hist(test_accuracys, color='skyblue', edgecolor='black')
+    # plt.title('Histogram')
+    # plt.xlabel('Value')
+    # plt.ylabel('Frequency')
+    # plt.grid(True)
+    # plt.show()
 
-    statistic, pvalue = scipy.stats.shapiro(test_accuracys)
-    print("statistic: ", statistic)
-    print("p-value: ", pvalue)
+    
     
     
     
