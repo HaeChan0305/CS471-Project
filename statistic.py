c34ac64ced46c0c60591f3da8ea67471bf273124
chan-K <kim.haechan@kaist.ac.kr>
2024-05-26 21:04:26 +0900

    images

--- statistic.py
+++ statistic.py
@@ -12,11 +12,10 @@ import matplotlib.pyplot as plt
 # args = parser.parse_args()
 
 def extract_test_accuracy(file):
-    with open(file, 'rb') as f:
-        byte_data = f.read()
-    str_data = byte_data.decode('utf-8')
-    str_data = str_data.split("Test accuracy: ")[1].strip()
-    test_accuracy = float(str_data)
+    with open(file, 'r') as f:
+        s  = f.read()
+        s = s.split("Test accuracy: ")[1].strip()
+        test_accuracy = float(s)
     return test_accuracy
 
 def check_validity(file):
@@ -36,10 +35,16 @@ if __name__ == "__main__":
             #         check_validity(os.path.join(dir, file)) 
     
             test_accuracys = [extract_test_accuracy(os.path.join(dir, file)) for file in os.listdir(dir)]
-            # statistic, pvalue = scipy.stats.shapiro(test_accuracys)
+            
+            # NCI1-ablation3
+            # test_accuracys = [0.7347931873479319, 0.7737226277372263, 0.6739659367396593, 0.7493917274939172, 0.708029197080292, 0.7201946472019465, 0.7712895377128953, 0.6642335766423357, 0.635036496350365, 0.7591240875912408, 0.6934306569343066, 0.7274939172749392, 0.7761557177615572, 0.7104622871046229, 0.6861313868613139, 0.7396593673965937, 0.7518248175182481, 0.7007299270072993, 0.754257907542579, 0.7104622871046229, 0.7274939172749392, 0.7347931873479319, 0.7493917274939172, 0.6690997566909975, 0.7445255474452555, 0.754257907542579, 0.7493917274939172, 0.7031630170316302, 0.732360097323601, 0.7226277372262774]
+
+            # NCI1-ablation2
+            # test_accuracys = [0.7250608272506083, 0.6763990267639902, 0.6909975669099757, 0.6909975669099757, 0.6958637469586375, 0.6739659367396593, 0.7274939172749392, 0.7299270072992701, 0.7128953771289538, 0.7688564476885644, 0.7591240875912408, 0.683698296836983, 0.7372262773722628, 0.6545012165450121, 0.6861313868613139, 0.708029197080292, 0.7226277372262774, 0.6739659367396593, 0.6763990267639902, 0.7128953771289538, 0.6447688564476886, 0.6934306569343066, 0.6788321167883211, 0.7031630170316302, 0.7055961070559611, 0.5766423357664233, 0.7396593673965937, 0.6861313868613139, 0.754257907542579, 0.7177615571776156]
+            
             print(f"{dataset}-ablation{ablation}")
             print("mean: ", np.mean(test_accuracys))
-            print("std: ", np.std(test_accuracys))
+            print("std: ", np.std(test_accuracys, ddof=1))
             print()
             print()
     
